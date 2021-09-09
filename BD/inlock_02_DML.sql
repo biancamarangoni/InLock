bf7c40575b3a8fd28b65125bdf85d3f5a131463e
Lucas-M3dina <Lucas.m3dina@gmail.com>
2021-09-09 19:42:27 -0300

    API - Domains

--- BD/inlock_02_DML.sql
+++ BD/inlock_02_DML.sql
@@ -11,7 +11,7 @@ INSERT INTO USUARIO (idTipoUsuario, email, senha)
 VALUES (1, 'admin@admin.com', 'admin'), (2, 'cliente@cliente.com', 'cliente');
 GO
 
-INSERT INTO ESTUDIOS (nomeEstudio)
+INSERT INTO ESTUDIO (nomeEstudio)
 VALUES ('Blizzard'), ('Rockstar Studios'), ('Square Enix');
 GO
 
